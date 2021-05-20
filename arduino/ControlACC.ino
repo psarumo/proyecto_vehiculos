48dde786fccec627e71e3d29d4f8ec5a20b71a4a
AndresHR97 <75427306+AndresHR97@users.noreply.github.com>
2021-05-20 13:58:54 +0200

    Update ControlACC.ino
    He metido la lectura por Bluetooth de la referencia, el tiempo de muestro y he terminado de implementar el cálculo de la señal de control y de las integrales.
    
    Faltaría ver cómo hay que implementar PWM

--- arduino/ControlACC.ino
+++ arduino/ControlACC.ino
@@ -1,14 +1,18 @@
-//Control por realimentación del vector de estados
+//Control por realimentación del vector de estados. Control de Vehiculos (MII/MIERA. 2020-2021)
 
-const int SensorPin=1;
 //Aqui irian los pines de los motores
-const float T=0; //Tiempo de muestreo
+const int SensorPin=2;
+
+//Constantes de control y del sistema
+const float T=0; //Tiempo de muestreo en milisegundos
 const float r=0; //Radio de la rueda
 const float K1=0; //Ganancia del controlador [K,KI]
 const float K2=0;
 const float K3=0;
 const float K4=0;
+const float ref_d = 0;
 
+//Variables de estados y señal de control
 float Dx3=0;
 float x1=0;
 float x2=0;
@@ -17,27 +21,54 @@ float x4=0;
 float w=0; //Velocidad angular
 float U=0; //Señal de control x4
 float U1=0; //Señal de control
-int x2_read=0;
-int x1_read=0;
+int vel_cp=0;
+
+//Variables auxiliares
+unsigned long t_0=0;
+unsigned long t_1=0;
+int count = 0;
+
 void setup() {
   
   Serial.begin (9600);
-  pinMode (SensorPin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(SensorPin),counter,RISING);
  
 }
 
-void loop() {
-  x2_read=analogRead(SensorPin);
-  w=0; // Relación Tensión - Medida
-  x2=w*r;
-  x1_read=0; // Se pasa el valor por Bluetooth
-  x1=0;
-  Dx3=r-x1; //Cuando se calcule KI en Matlab hay que cambiar el signo
-  x3+=Dx3*T;
-  x4+=x3*T;
+void counter(){
+   count++;
+}
 
-  U=-K1*x1-K2*x2+K3*x3+K4*x4;
-  U1=U/4.0; //U1 es tracción. Hay que pasar a señal PWM.
+void loop() {
+  //Lectura Bluetooth. Paso de la referencia desde el ordenador hasta el vehiculo
+  if (Serial.available() > 0) {
+      String buff = "";
+      while (Serial.available() > 0) {
+        buff += (char)Serial.read();
+      }
+      vel_cp = buff.toInt();
+    }
+
   
-  delay(300);
+  t_1=millis();
+  
+  if((t_1-t_0)>=T){
+    //Lectura de la velocidad y conversion a rev/s
+    w = ((float) count/20.0)/((t_1-t_0)/1000.0);
+    count = 0;
+
+    x2=w*r;
+    x1 += ((t_1-t_0)/1000.0)*(vel_cp-x2); //Calculo de x1 realizando la integral
+    Dx3=ref_d-x1; //Cuando se calcule KI en Matlab hay que cambiar el signo
+    x3+=Dx3*((t_1-t_0)/1000.0);
+    x4+=x3*((t_1-t_0)/1000.0);
+  
+    U=-K1*x1-K2*x2+K3*x3+K4*x4;
+
+    //Habria que ver como se implementa el PWM
+    U1=U/4.0; //U1 es tracción. Hay que pasar a señal PWM.
+
+    t_0=t_1; //Actualizamos el valor
+  }
+  delay(10); //No se si esto habria que quitarlo
 }
