a8b3e412810850dd6304ddaa806038f667ace24f
AndresHR97 <75427306+AndresHR97@users.noreply.github.com>
2021-06-12 23:21:44 +0200

    Update ControlACC_adaptado.ino

--- arduino/ControlACC_adaptado/ControlACC_adaptado.ino
+++ arduino/ControlACC_adaptado/ControlACC_adaptado.ino
@@ -46,13 +46,14 @@ float error = 0;
 float int_error = 0;
 int pwm = 200;
 float Ai = 0;
-float ref_v = 0.5; // Velocidad vehiculo de delante
+float ref_v = 0.4; // Velocidad vehiculo de delante
 
 /* Variables auxiliares */
 unsigned long t_0 = 0;
 unsigned long t_1 = 0;
 unsigned long t_start = 0;
 int count = 0;
+bool arranque = false;
 bool change= false; //La idea es pasar esto por bluetooh o progamarlo para que cambie al cabo de cierto tiempo
                //para asegurarse que se ha alcanzado la velocidad de consigna
 
@@ -93,85 +94,96 @@ void counter() {
 }
 
 void loop() {
-  /* Lectura Bluetooth. Paso de la referencia desde el ordenador hasta el vehiculo */
-
-  if (BT.available()) {
-    // Leer un dato (esto seria vref). No podemos printear pedir d ese guarda en el archivo y no es lo que queremos
-    char c = BT.read();
-    if (c >= '0' && c <= '9') data += c; // Control de error
-
-    if (c == '\r') {
-      if (data.length() > 0){
-      ref_v = data.toFloat();
-      last_data = data;
-      }
-      data = "";
-    }
-  }
-
-
-  t_1 = millis();
-
-  if ((t_1 - t_0) >= T) { // En cada instante de muestreo
-
-    /* Obtener velocidad */
-    // Lectura de la velocidad y conversion a rev/s
-    w_leida = ((float) count * 2 * 3.1416 / 20.0) / ((t_1 - t_0) / 1000.0);
-    //count = 0;
-
-    // Filtrar lectura
-    if (w - w_leida > 1 || w_leida - w > 1) { //No se si este filtro puede dar problemas en caso de que la primera lectura no sea cercana a 0
-      w = 0.2 * w_leida + 0.8 * w;
-    } else w = w_leida;
-
-    // Velocidad lineal
-    v = w * r;
-
-    count = 0;
-
-    /* Control */
-  /*  if(change==false){
-      x1=0; //Se resetean las variables acumulativas del control de distancia
-      x3=0;
-      x4=0;
-      error=ref_v-(w*r);
-      int_error+=error*((t_1-t_0)/1000.0);
-      Ai=Ki*int_error;
-      //Antiwindup
-      if (Ai>=Aimax){
-        Ai=Aimax;}
-      else if(Ai<=Aimin){
-        Ai=Aimin;} 
-      //Señal de control
-      pwm=Kp*error+Ai;
-    }
-    else{*/
-     // int_error=0;//Se resetea la integral del error de velocidad
-      x2=ref_v-w*r; //x2 debe ser 0 al inicio
-      x1 += ((t_1-t_0)/1000.0)*(ref_v-(w*r)); //Calculo de x1 realizando la integral
-      Dx3=ref_d-x1;
-      x3+=Dx3*((t_1-t_0)/1000.0);
-      x4+=x3*((t_1-t_0)/1000.0);
-     
-  
-      pwm=(int)(-K1*x1-K2*x2+K3*x3+K4*x4+120.0);
-     
-     if(pwm>255) pwm=255;
-     if(pwm<120) pwm=120;
-  //}
-
-    /* Aplicar seniales */
+   if(arranque==false){
+      pwm = 255;
+   /* Aplicar seniales */
     analogWrite (motorI1, pwm);
     analogWrite (motorD3, pwm);
+         if(millis()>5000){
+            arranque = true;
+            pwm = 150;
+         }
+   }
+   else{
+     /* Lectura Bluetooth. Paso de la referencia desde el ordenador hasta el vehiculo */
 
-    /* Escribir datos */
-    linea = String(pwm) + "," + String(v, 4) + "," + last_data + "," + String(t_1 - t_start) + "\n\r"; // pwm, v, vref, t
-    BT.write(linea.c_str());
+     if (BT.available()) {
+       // Leer un dato (esto seria vref). No podemos printear pedir d ese guarda en el archivo y no es lo que queremos
+       char c = BT.read();
+       if (c >= '0' && c <= '9') data += c; // Control de error
 
-    t_0 = t_1; //Actualizamos el valor
+       if (c == '\r') {
+         if (data.length() > 0){
+         ref_v = data.toFloat();
+         last_data = data;
+         }
+         data = "";
+       }
+     }
 
-  }
 
-  delay (10); //0.3s
+     t_1 = millis();
 
+     if ((t_1 - t_0) >= T) { // En cada instante de muestreo
+
+       /* Obtener velocidad */
+       // Lectura de la velocidad y conversion a rev/s
+       w_leida = ((float) count * 2 * 3.1416 / 20.0) / ((t_1 - t_0) / 1000.0);
+       //count = 0;
+
+       // Filtrar lectura
+       if (w - w_leida > 1 || w_leida - w > 1) { //No se si este filtro puede dar problemas en caso de que la primera lectura no sea cercana a 0
+         w = 0.2 * w_leida + 0.8 * w;
+       } else w = w_leida;
+
+       // Velocidad lineal
+       v = w * r;
+
+       count = 0;
+
+       /* Control */
+     /*  if(change==false){
+         x1=0; //Se resetean las variables acumulativas del control de distancia
+         x3=0;
+         x4=0;
+         error=ref_v-(w*r);
+         int_error+=error*((t_1-t_0)/1000.0);
+         Ai=Ki*int_error;
+         //Antiwindup
+         if (Ai>=Aimax){
+           Ai=Aimax;}
+         else if(Ai<=Aimin){
+           Ai=Aimin;} 
+         //Señal de control
+         pwm=Kp*error+Ai;
+       }
+       else{*/
+        // int_error=0;//Se resetea la integral del error de velocidad
+         x2=ref_v-w*r; //x2 debe ser 0 al inicio
+         x1 += ((t_1-t_0)/1000.0)*(ref_v-(w*r)); //Calculo de x1 realizando la integral
+         Dx3=ref_d-x1;
+         x3+=Dx3*((t_1-t_0)/1000.0);
+         x4+=x3*((t_1-t_0)/1000.0);
+
+
+         pwm=(int)(-K1*x1-K2*x2+K3*x3+K4*x4+120.0);
+
+        if(pwm>255) pwm=255;
+        if(pwm<120) pwm=120;
+     //}
+
+       /* Aplicar seniales */
+       analogWrite (motorI1, pwm);
+       analogWrite (motorD3, pwm);
+
+       /* Escribir datos */
+       linea = String(pwm) + "," + String(v, 4) + "," + last_data + "," + String(t_1 - t_start) + "\n\r"; // pwm, v, vref, t
+       BT.write(linea.c_str());
+
+       t_0 = t_1; //Actualizamos el valor
+
+     }
+
+     delay (10); //0.3s
+   }
 }
